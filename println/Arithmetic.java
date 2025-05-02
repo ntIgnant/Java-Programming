1e5100be63928a0c0395cace10b3b9bb35dba49b
ntIgnant <ignanttt@gmail.com>
2025-05-02 21:19:30 +0200

    UPDATED | basic arithmetic in java

---
+++ println/Arithmetic.java
@@ -0,0 +1,14 @@
+public class Arithmetic {
+    public static void main(String[] args){
+        // This is gonna be for basic arithmetic stuff
+
+        int num1 = 5;
+        int num2 = 10;
+
+        int result = num1 + num2;
+
+        System.out.printf("The result of %d + %d = %d\n", num1, num2, result);
+        
+
+    }
+}
