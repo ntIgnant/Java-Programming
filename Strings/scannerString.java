b8139fda01546c55ca3fce861a001a0d6bb7cd9b
zigz <ignanttt@gmail.com>
2026-01-01 20:53:52 +0100

    UPDATED | basic input handling in java | java recap

--- Strings/scannerString.java
+++ Strings/scannerString.java
@@ -4,9 +4,15 @@ import java.util.Scanner;
 public class scannerString {
 
     public static void main(String[] args){
-        System.out.println("Hi! What's your name: ");
+        System.out.print("Hi! What's your name: ");
 
         // Create a new scanner for the user input
-        String userName = Scanner
+        Scanner input = new Scanner(System.in); // Create a new scanner oject for the input
+        String userName = input.nextLine(); // Get the actuall string (name) from the user
+
+        // Close the scanner object
+        input.close();
+
+        System.out.println("Hello " + userName + " what's good!");
     }
 }
