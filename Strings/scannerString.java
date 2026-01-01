ae0ebf32deba2786c46a703cbd2ad5d49940dd4f
zigz <ignanttt@gmail.com>
2026-01-01 19:50:09 +0100

    UPDATED | scanner basics in java

---
+++ Strings/scannerString.java
@@ -0,0 +1,12 @@
+// Import the Scaner utility in order to use Scanner (for input)
+import java.util.Scanner;
+
+public class scannerString {
+
+    public static void main(String[] args){
+        System.out.println("Hi! What's your name: ");
+
+        // Create a new scanner for the user input
+        String userName = Scanner
+    }
+}
