bd58fdd1a5d405e500ee03f6a6ff06052a2c82d2
zigz <ignanttt@gmail.com>
2026-01-05 11:09:49 +0100

    UPDATED | directory names changed | functions ands classes basics in java

---
+++ Classes/BuildTriangle.java
@@ -0,0 +1,9 @@
+public class BuildTriangle {
+
+    public static void main(String[] args){
+        // Create new Triangles (instances of the created class Triangle)
+        Triangle triangle1 = new Triangle(15, 8, 15, 17);
+        Triangle triangle1 = new Triangle(3, 2.5, 9, 8);
+        Triangle trin
+    }
+}
