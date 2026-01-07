bdd195491d3351b3cb8bc550b91e91b28aeb0eb9
ignanttt <ignanttt@gmail.com>
2026-01-07 23:46:49 +0100

    UPDATED | triangle object created

--- Classes/BuildTriangle.java
+++ Classes/BuildTriangle.java
@@ -2,6 +2,8 @@ public class BuildTriangle {
 
     public static void main(String[] args){
 
+        // Create two instances of the Triangle object
         Triangle tria1 = new Triangle(17, 11);
+        Triangle tria2 = new Triangle(5, 11);
     }
 }
--- Classes/Triangle.java
+++ Classes/Triangle.java
@@ -5,8 +5,12 @@ public class Triangle {
     private double height;
 
     // Create the builder of Triangle
-    public Triangle(boolean base, boolean height){
+    public Triangle(double base, double height){
         this.base = base;
         this.height = height;
+    }
+
+    public double getArea(){
+        return (this.base * this.height) / 2; // Return the area of the triangle
     }
 }
