537958d9db827c95b7b4f9674490dc6b0a816410
ignanttt <ignanttt@gmail.com>
2026-01-10 18:07:15 +0100

    UPDATED | scr directory fixed

--- Src/TryOut/BuildSquare.java
+++ Src/TryOut/BuildSquare.java
@@ -5,9 +5,13 @@ public class BuildSquare {
 
         // Create a new instance of the object square
         Square sqr1 = new Square(5);
+        Square sqr2 = new Square(10);
+        Square sqr3 = new Square(3);
 
         double sqr1_area = sqr1.getSquareArea();
 
         System.out.println("Square1 with side " + sqr1.side + " has area " + sqr1_area);
+
+        for(int i = 1)
     }
 }
--- Src/TryOut/Square.java
+++ Src/TryOut/Square.java
@@ -5,10 +5,12 @@ public class Square {
     public double side;
 
     public Square(double side){
+
         this.side = side;
     }
 
     public double getSquareArea(){
         return (side * side);
     }
+
 }
