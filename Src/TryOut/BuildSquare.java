da74066fa33f9172da4b4bd822d055e26ae3bbf0
ignanttt <ignanttt@gmail.com>
2026-01-10 19:10:21 +0100

    UPDATED | square task done | for loop in fava recap

--- Src/TryOut/BuildSquare.java
+++ Src/TryOut/BuildSquare.java
@@ -1,4 +1,5 @@
 package TryOut;
+import java.util.ArrayList;
 
 public class BuildSquare {
     public static void main(String[] args){
@@ -8,10 +9,20 @@ public class BuildSquare {
         Square sqr2 = new Square(10);
         Square sqr3 = new Square(3);
 
+        // Create and Add all the class instances (objects) to the arrayList
+        ArrayList<Square> squares = new ArrayList<Square>();
+
+        // Add the square objects to the ArrayList
+        squares.add(sqr1);
+        squares.add(sqr2);
+        squares.add(sqr3);
+
         double sqr1_area = sqr1.getSquareArea();
 
-        System.out.println("Square1 with side " + sqr1.side + " has area " + sqr1_area);
+//        System.out.println("Square1 with side " + sqr1.side + " has area " + sqr1_area);
 
-        for(int i = 1)
+        for (int i = 0; i < 3; i++){
+            System.out.println("Square" + i+1 + " with side " + squares.get(i) + " has area " + squares.get(i).getSquareArea());
+        }
     }
 }
